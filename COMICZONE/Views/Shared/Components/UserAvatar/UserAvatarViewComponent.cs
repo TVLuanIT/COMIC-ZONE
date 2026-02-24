c6ba632869973ea0157d42186ae71397e274926f
TVLuanTI <luan31032004@gmail.com>
2026-02-24 13:47:06 +0700

    feat(productreview): add reply form for ProductReviewList

---
+++ COMICZONE/Views/Shared/Components/UserAvatar/UserAvatarViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMICZONE.Helpers
+{
+    public class UserAvatarViewComponent : ViewComponent
+    {
+        // Truyền trực tiếp avatarUrl và size
+        public IViewComponentResult Invoke(string? avatarUrl, int size = 32)
+        {
+            ViewData["AvatarUrl"] = avatarUrl;
+            ViewData["Size"] = size;
+            return View();
+        }
+    }
+}
