de071b01ea3db40015de7d23e4ac5b9aafda559b
TVLuanTI <luan31032004@gmail.com>
2026-03-06 22:17:30 +0700

    feat(user-profile): add MyProfile layout with sidebar and partial views

---
+++ COMICZONE/Controllers/UserProfilesController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using COMICZONE.Data;
+using COMICZONE.Models;
+
+namespace COMICZONE.Controllers
+{
+    public class UserProfilesController : BaseController
+    {
+        private readonly ComiczoneContext _context;
+
+        public UserProfilesController(ComiczoneContext context)
+        {
+            _context = context;
+        }
+
+        private Customer? GetCustomer()
+        {
+            var userIdStr = HttpContext.Session.GetString("UserId");
+
+            if (string.IsNullOrEmpty(userIdStr))
+            {
+                return null;
+            }
+
+            int userId = int.Parse(userIdStr);
+
+            var customer = _context.Customers
+                .Include(c => c.User)
+                .FirstOrDefault(c => c.Userid == userId);
+
+            return customer;
+        }
+
+        public IActionResult MyProfile()
+        {
+            var customer = GetCustomer();
+
+            return View(customer);
+        }
+
+        public IActionResult MyOrders()
+        {
+            var customer = GetCustomer();
+
+            return View(customer);
+        }
+
+        public IActionResult MyReviews()
+        {
+            var customer = GetCustomer();
+
+            return View(customer);
+        }
+
+    }
+}
